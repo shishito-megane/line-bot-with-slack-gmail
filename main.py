aa92fc2ab5720578d84cb855daf9799456528d21
shishito.megane my_ubuntu <shishito.megane3060.3@gmail.com>
2018-04-30 11:26:29 +0900

    実行形式を変更

--- main.py
+++ main.py
@@ -76,4 +76,3 @@ def message_text(event):
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    app.run(debug=options.debug, port=options.port)
